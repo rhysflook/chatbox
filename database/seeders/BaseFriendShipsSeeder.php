40f60fdb5d63e02679474721770a2ee9c7bf2691
Rhys Flook <rhys@media-system.co.jp>
2023-10-20 15:41:01 +0900

    edit migration, factory and seeder to add username field

--- database/seeders/BaseFriendShipsSeeder.php
+++ database/seeders/BaseFriendShipsSeeder.php
@@ -14,7 +14,7 @@ class BaseFriendShipsSeeder extends Seeder
      */
     public function run(): void
     {
-        $rhys = User::create(['name' => 'Rhys', 'email' => 'rhys@media-system.co.jp', 'password' => \Hash::make('password')]);
+        $rhys = User::create(['name' => 'Rhys', 'username' => 'ride_or_die_hotdog', 'email' => 'rhys@media-system.co.jp', 'password' => \Hash::make('password')]);
         User::factory()->count(10)->create();
         $other_users = User::where('name', '<>', 'Rhys')->get();
         foreach ($other_users->all() as $user) {
